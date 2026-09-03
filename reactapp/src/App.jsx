63c1f073e7522120380aa6b8eeb73b37a0df9d16
AnushaLakshmi-r <anu.r.lakshmi98@gmail.com>
2026-09-03 10:59:50 +0530

    first commit

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+function TodoList() {
+ const [todos, setTodos] = useState([
+  { id: 1, task: "Learn React", completed: false },
+ { id: 2, task: "Build a project", completed: true },
+ ]);
+ const toggleComplete = (id) => {
+ setTodos(
+ todos.map((todo) =>
+ todo.id === id ? { ...todo, completed: !todo.completed } : todo
+ )
+ );
+ };
+ return (
+ <ul>
+ {todos.map((todo) => (
+ <li
+ key={todo.id}
+ style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+ onClick={() => toggleComplete(todo.id)}
+ >
+ {todo.task}
+ </li>
+ ))}
+ </ul>
+ );
+}
